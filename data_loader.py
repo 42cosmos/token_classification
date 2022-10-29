c9ffbeaee6420766045b04fa5db39c556da9adf3
Eunbin Park <cosmos.42@icloud.com>
2022-10-29 19:28:23 +0900

    insert preprocess to data loader

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,25 @@
+import torch
 import numpy as np
 from functools import partial
 from datasets import load_dataset
 from easydict import EasyDict
-from torch.utils.data import Dataset
+from torch.utils.data import TensorDataset
 from transformers import AutoTokenizer
+
+from utils import LABEL_MAPPING
+
+
+class InputFeatures:
+    def __init__(self, input_ids, attention_mask, token_type_ids, label_ids):
+        self.input_ids = input_ids
+        self.attention_mask = attention_mask
+        self.token_type_ids = token_type_ids
+        self.label_ids = label_ids
 
 
 class DataLoader:
     def __init__(self, CFG):
+        self.config = CFG
         self.dset_name = CFG.dset_name
         self.task = CFG.task
         self.PLM = CFG.PLM
@@ -15,12 +27,18 @@ class DataLoader:
         self.max_length = CFG.max_token_length
         self.seed = CFG.seed
 
-    def load(self):
-        dataset = load_dataset(self.dset_name, self.task)
+    def load(self, mode):
+        dataset = load_dataset(self.dset_name, self.task, split=mode)
         tokenizer = AutoTokenizer.from_pretrained(self.PLM)
         encode_fn = partial(self.label_tokens_ner, tokenizer=tokenizer)
-        dataset = dataset.map(encode_fn, batched=False)
-        dataset = dataset.remove_columns(column_names=["sentence", "tokens", "ner_tags", "offset_mapping"])
+        features = list(map(encode_fn, dataset))
+
+        all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+        all_attention_mask = torch.tensor([f.attention_mask for f in features], dtype=torch.long)
+        all_token_type_ids = torch.tensor([f.token_type_ids for f in features], dtype=torch.long)
+        all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
+
+        dataset = TensorDataset(all_input_ids, all_attention_mask, all_token_type_ids, all_label_ids)
 
         return dataset
 
@@ -28,12 +46,11 @@ class DataLoader:
         sentence = "".join(examples["tokens"])
         tokenized_output = tokenizer(
             sentence,
-            return_token_type_ids=False,
+            return_token_type_ids=True,
             return_offsets_mapping=True,
-            max_length=self.max_length,
-            truncation=True,
-        )
-
+            max_length=self.config.max_token_length,
+            padding=self.config.padding,
+            truncation=True)
         label_token_map = []
 
         list_label = examples["ner_tags"]
@@ -53,8 +70,19 @@ class DataLoader:
                 token_label = label_end if label_end != 12 else 12
 
             label_token_map.append(token_label)
-            # else:
-            #   raise ValueError("label_begin != label_end")
 
-        tokenized_output["labels"] = label_token_map
-        return tokenized_output
+        tokenized_output["labels"] = [LABEL_MAPPING[t] for t in label_token_map]
+
+        input_ids = tokenized_output["input_ids"]
+        attention_mask = tokenized_output["attention_mask"]
+        token_type_ids = tokenized_output["token_type_ids"]
+        label_ids = tokenized_output["labels"]
+
+        features = InputFeatures(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            label_ids=label_ids
+        )
+
+        return features
