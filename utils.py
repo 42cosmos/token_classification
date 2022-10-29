d54a4defdd70a1b64b03462e586b7337fd07659e
Eunbin Park <cosmos.42@icloud.com>
2022-10-30 00:50:50 +0900

    add get_test_texts

--- utils.py
+++ utils.py
@@ -1,25 +1,36 @@
+import os
 import logging
 
-
 LABEL_MAPPING = {
-            0: 0,       # B-PS
-            1: 0,
-            2: 1,       # B-LC
-            3: 1,
-            4: 2,       # B-OG
-            5: 2,
-            6: 3,       # B-DT
-            7: 3,
-            8: 4,       # B-IT
-            9: 4,
-            10: 5,      # B-QT
-            11: 5,
-            12: 6,      # O
-            -100: -100, # LABEL_PAD_TOKEN
-        }
+    0: 0,  # B-PS
+    1: 0,
+    2: 1,  # B-LC
+    3: 1,
+    4: 2,  # B-OG
+    5: 2,
+    6: 3,  # B-DT
+    7: 3,
+    8: 4,  # B-IT
+    9: 4,
+    10: 5,  # B-QT
+    11: 5,
+    12: 6,  # O
+    -100: -100,  # LABEL_PAD_TOKEN
+}
 
 
 def init_logger():
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S',
                         level=logging.INFO)
+
+
+def get_test_texts(args):
+    texts = []
+    with open(os.path.join(args.data_dir, args.test_file), 'r', encoding='utf-8') as f:
+        for line in f:
+            text, _ = line.split('\t')
+            text = text.split()
+            texts.append(text)
+
+    return texts
