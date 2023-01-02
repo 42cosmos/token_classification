58c85dc06d0d3f79093a83a0fd9ff90d1a55159f
Eunbin Park <parkeb417@gmail.com>
2023-01-02 12:34:04 +0900

    remove train code

--- run.sh
+++ run.sh
@@ -2,16 +2,4 @@
 mkdir token_data
 aws s3 sync s3://{my_bucket_name}/{my_object_name} ./token_data --no-sign-request
 
-unzip ./token_data/klue/klue.zip -d ./token_data/klue/
-rm -rf ./token_data/klue/klue.zip
-
-python3 main.py \
-	--do_train \
-	--do_eval \
-	--dset_name klue
-
-python3 main.py \
-	--do_train \
-	--do_eval \
-	--load_checkpoint \
-	--dset_name docent
+python3 inference.py --dset_name klue
