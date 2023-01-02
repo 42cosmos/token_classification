9e344db29485773391b953c82c014fcdf5f9a9d4
Eunbin Park <parkeb417@gmail.com>
2023-01-02 12:31:53 +0900

    add kss sentence splitter

--- data_loader.py
+++ data_loader.py
@@ -1,30 +1,56 @@
 import os
-import glob
+import logging
+import pandas as pd
 
 import torch
-import numpy as np
-from functools import partial
-from datasets import load_dataset
-from easydict import EasyDict
-from torch.utils.data import TensorDataset
-from transformers import AutoTokenizer
+
+import pyarrow as pa
+import pyarrow.dataset as ds
+from datasets import load_dataset, Dataset, concatenate_datasets
+
+from tqdm import tqdm
 
 from utils import LABEL_MAPPING
-
-import logging
 
 logger = logging.getLogger(__name__)
 
 
+class DocentDataset:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.assign_sentence_and_label()
+        self.phrase_to_sentence()
+
+    def assign_sentence_and_label(self):
+        self.original_phrase = [''.join(row) for row in self.dataset['tokens']]
+        self.original_label = [row for row in self.dataset['ner_tags']]
+
+    def phrase_to_sentence(self):
+        self.sentence = []
+        self.label = []
+        error = 0
+        for phrase, labels in tqdm(zip(self.original_phrase, self.original_label), total=len(self.original_phrase)):
+            try:
+                sentences = kss.split_sentences([" " if i.strip() == "" else i.strip() for i in phrase], strip=False)
+            except:
+                error += 1
+                print(f'Error Occurred : {error}')
+            use_len_sentence = 0
+            for sentence in sentences:
+                self.sentence.append(sentence)
+                self.label.append(labels[use_len_sentence:use_len_sentence + len(sentence)])
+                use_len_sentence += len(sentence)
+
+
 class Loader:
-    def __init__(self, CFG, tokenizer):
-        self.config = CFG
-        self.dset_name = CFG.dset_name
-        self.task = CFG.task
-        self.model_name_or_path = CFG.model_name_or_path
-        self.batch_size = CFG.train_batch_size
-        self.max_length = CFG.max_token_length
-        self.seed = CFG.seed
+    def __init__(self, config, tokenizer):
+        self.config = config
+        self.dset_name = config.dset_name
+        self.task = config.task
+        self.model_name_or_path = config.model_name_or_path
+        self.batch_size = config.train_batch_size
+        self.max_length = config.max_token_length
+        self.seed = config.seed
 
         self.tokenizer = tokenizer
 
@@ -33,15 +59,33 @@ class Loader:
         cached_file_name = f"cached_{self.dset_name}_{dataset_type}-{model_info}"
         cached_features_file = os.path.join(self.config.data_dir, cached_file_name)
 
+        docent_cached_file_name = f"cached_docent_{dataset_type}-koelectra-base-v3-discriminator"
+        docent_cached_features_file = os.path.join(self.config.data_dir, docent_cached_file_name)
+
         if os.path.exists(cached_features_file):
             logger.info(f"Loading features from cached file {cached_features_file}")
             dataset = torch.load(cached_features_file)
+
+            if dataset_type == "train" or dataset_type == "validation":
+                if not os.path.exists(cached_features_file):
+                    klue_dataset = load_dataset(path=os.path.join(self.config.data_dir, "klue"),
+                                                split=dataset_type)
+
+                    col_name = klue_dataset.column_names
+                    klue_dataset = klue_dataset.map(self.tokenize_and_align_labels, batched=False)
+                    klue_dataset = klue_dataset.remove_columns(col_name)
+
+                    dataset = concatenate_datasets([klue_dataset, dataset])
+
+
         else:
-            dataset = load_dataset(path=os.path.join(self.config.data_dir, self.config.dset_name),
+            dataset = load_dataset(path=os.path.join(self.config.data_dir, self.dset_name),
                                    split=dataset_type)
 
+            col_name = dataset.column_names
             dataset = dataset.map(self.tokenize_and_align_labels, batched=False)
-            dataset = dataset.remove_columns(["tokens", "ner_tags", "sentence", "offset_mapping"])
+            dataset = dataset.remove_columns(col_name)
+
             torch.save(dataset, cached_features_file)
             logger.info(f"Saved features into cached file {cached_features_file}")
 
@@ -51,31 +95,29 @@ class Loader:
         sentence = "".join(examples["tokens"])
         tokenized_output = self.tokenizer(
             sentence,
-            return_token_type_ids=True,
-            return_offsets_mapping=True,
+            return_token_type_ids=True if self.config.use_token_types else False,
+            return_offsets_mapping=False,
             max_length=self.max_length,
             truncation=True,
         )
 
-        label_token_map = []
-
         list_label = examples["ner_tags"]
-        list_label = [-100] + list_label + [-100]
-
-        for token_idx, offset_map in enumerate(tokenized_output["offset_mapping"]):
-            begin_letter_idx, end_letter_idx = offset_map
-            label_begin = list_label[begin_letter_idx]
-            label_end = list_label[end_letter_idx]
-            token_label = np.array([label_begin, label_end])
-            if label_begin == 12 and label_end == 12:
-                token_label = 12
-            elif label_begin == -100 and label_end == -100:
-                token_label = -100
+        tokenized_sentence = [word for word in self.tokenizer.tokenize(sentence)]
+        sub_word_based_label = []
+        start_tokenized_word_idx = 0
+        sub_word = ''
+        for idx, (char_tok, char_lbl) in enumerate(zip(sentence, list_label)):
+            if char_tok == ' ':
+                continue
             else:
-                token_label = label_begin if label_begin != 12 else 12
-                token_label = label_end if label_end != 12 else 12
+                sub_word += char_tok
+            if sub_word == tokenized_sentence[start_tokenized_word_idx].replace('##', ''):
+                start_tokenized_word_idx += 1
+                sub_word_based_label.append(list_label[idx - len(sub_word) + 1])
+                sub_word = ''
 
-            label_token_map.append(token_label)
+        # based_label = [LABEL_MAPPING[i] for i in sub_word_based_label[:self.max_length - 2]]
+        sub_word_based_label = [-100] + sub_word_based_label[:self.max_length - 2] + [-100]
 
-        tokenized_output["labels"] = label_token_map
+        tokenized_output["labels"] = sub_word_based_label
         return tokenized_output
